055a05a54a5005941795bcb871baaee1d292c2a7
Aleksander <spas.aleksander.v@gmail.com>
2022-12-30 14:14:56 +0300

    Задание выволнено

--- Урок 1. Знакомство с языком программирования С#/Задача 2/Program.cs
+++ Урок 1. Знакомство с языком программирования С#/Задача 2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.Write("Введите два числа: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Наибольшее число - '{num1}'");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число - '{num2}'");
+}
