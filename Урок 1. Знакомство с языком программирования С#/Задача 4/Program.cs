58b28183d99bd96965b19991212ebb895c781311
Aleksander <spas.aleksander.v@gmail.com>
2022-12-30 15:59:19 +0300

    Задание выволнено

--- Урок 1. Знакомство с языком программирования С#/Задача 4/Program.cs
+++ Урок 1. Знакомство с языком программирования С#/Задача 4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+Console.Write("Введите три числа: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+int num3 = int.Parse(Console.ReadLine() ?? "");
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+else if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine($"Наибольшее число - '{max}'");
