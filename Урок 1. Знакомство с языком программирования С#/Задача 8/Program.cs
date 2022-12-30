e1c066d5580733d82b1e9faf5e396d3f503b89d8
Aleksander <spas.aleksander.v@gmail.com>
2022-12-30 16:48:24 +0300

    Задание выволнено

--- Урок 1. Знакомство с языком программирования С#/Задача 8/Program.cs
+++ Урок 1. Знакомство с языком программирования С#/Задача 8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine() ?? "");
+
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
